b9b75ec4633e63388ab40d292fe4e28efff072c9
Chaos <stefan@binaervarianz.de>
2014-09-20 18:10:36 +0200

    added outline

--- gcoderesume.py
+++ gcoderesume.py
@@ -4,7 +4,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Read and modify Gcode')
 
-parser.add_argument('filename', action='store', type=argparse.FileType('a'), help='Gcode-File to be altered')
+parser.add_argument('filename', action='store' help='Gcode-File to be altered')
 
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('-l', '--layer',  action='store', type=int, metavar='layer', help='The layer at which to continue printing')
@@ -12,4 +12,15 @@ group.add_argument('-z', '--zheight', action='store', type=int, metavar='height'
 
 args = parser.parse_args()
 
-print args
+#todo:
+#search for height if possible
+#get layer number
+#get height
+#get extrusion of last layer
+#get xy position of first point
+#get post-header code point
+#delete non-wanted code
+#insert post-init move to height + safety
+#insert extrusion set to last layers value
+#save file
+#return x,y of first point to calling program
