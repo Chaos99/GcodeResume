0d68f511d5343d339839bc9c3bd7bda66f200d7d
Chaos <stefan@binaervarianz.de>
2014-09-20 19:10:48 +0200

    get last extrusion value for given layer

--- gcoderesume.py
+++ gcoderesume.py
@@ -7,8 +7,8 @@ parser = argparse.ArgumentParser(description='Read and modify Gcode')
 parser.add_argument('filename', action='store', help='Gcode-File to be altered')
 
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument('-l', '--layer',  action='store', type=int, metavar='layer', help='The layer at which to continue printing')
-group.add_argument('-z', '--zheight', action='store', type=float, metavar='height', help='The height at which to continue printing')
+group.add_argument('-l', '--layer',  action='store', type=int, default=None, metavar='layer', help='The layer at which to continue printing')
+group.add_argument('-z', '--zheight', action='store', type=float, default=None, metavar='height', help='The height at which to continue printing')
 
 args = parser.parse_args()
 
@@ -16,16 +16,49 @@ args = parser.parse_args()
 
 #get post-header code point
 startLayer0 = None
+endOfLastLine = None
+endOfSecondToLastLine = None
+repeat = False
+extrude = None
+layer = None
 
-with open(args.filename) as f:
-    lastpos = f.tell()
-    for line in f:        
+with open(args.filename) as f:   
+    for line in f:   
+        #remember positions of last 3 lines
+        endOfThirdToLastLine = endOfSecondToLastLine
+        endOfSecondToLastLine = endOfLastLine
+        endOfLastLine = f.tell()
         if ';LAYER:0' in line:
-            startLayer0 = lastpos            
-        lastpos = f.tell()
+            #get position to cut later on
+            startLayer0 = endOfLastLine 
+        if ';LAYER:' in line:   
+            #security block, because we will come back to this after the backtracking
+            if repeat: 
+                repeat = False
+                continue
+            # get current layer number
+            layer = int(line.split(':')[1].strip())
+            
+            # if layer number given as parameter, search for it
+            if args.layer and layer == args.layer:
+                repeat = True
+                #jump back to last line
+                f.seek(endOfSecondToLastLine)
+                lastLine = f.readline() #reads just the newline char
+                lastLine = f.readline()
+                if ('G1' in lastLine or 'G0'in lastLine) and 'E' in lastLine:
+                    command = lastLine.split(' ')
+                    for part in command:
+                        if 'E' in part:
+                            #get the last extrusion value
+                            extrude=float(part[1:])
+                
+        endOfLastLine = f.tell()
 
 if startLayer0:
     print startLayer0
+if extrude:
+    print extrude
     
 
 #search for height if possible
