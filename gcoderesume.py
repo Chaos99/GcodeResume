e73866d8caa871131443da9a71375d90dc2a7ff5
Chaos <stefan@binaervarianz.de>
2014-09-21 01:51:32 +0200

    finished first prototype

--- gcoderesume.py
+++ gcoderesume.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import tempfile
+from sys import exit
+from os import remove
+from shutil import copy
 
 parser = argparse.ArgumentParser(description='Read and modify Gcode')
 
@@ -75,31 +79,31 @@ class rewindable_iterator(object):
         
     def seek(self, num):
         return self._iter.seek(num)
-#todo:
 
-startLayer0 = None
-cutPosition = None
-repeat = False
 extrude = None
 layer = None
-height = None
+height = 0.0
 xCoord = None
 yCoord = None
+stop = False
 
-with open(args.filename, mode='r+t') as f:  
+#create temp file to copy content line by line
+w = tempfile.NamedTemporaryFile(mode='w+t', delete=False)
+temp_name = w.name
+
+#open original for reading
+with open(args.filename, mode='rt') as f:  
     #re-instantiate as iterator with look-back and look-ahead
     fplus = rewindable_iterator(f)
-    for line in fplus:   
-        endOfLastLine = fplus.tell()
+    for line in fplus:    
 #get post-header code point
         if ';LAYER:0' in line:
-            #get position to cut later on
-            startLayer0 = endOfLastLine 
-        if ';LAYER:' in line:              
-            # get current layer number
-            layer = int(line.split(':')[1].strip())
-            cutPosition = endOfLastLine
+            #stop copying line to new file 
+            stop = True
             
+        if stop and ';LAYER:' in line:              
+            # get current layer number
+            layer = int(line.split(':')[1].strip())            
 #search for height
             nextLine = fplus.ahead()
             if ('G1' in nextLine or 'G0'in nextLine) and 'Z' in nextLine:
@@ -109,7 +113,8 @@ with open(args.filename, mode='r+t') as f:
                         #get the next height value
                         height=float(part[1:])
 #get extrusion of last layer
-            lastLine = fplus.last()                
+            lastLine = fplus.last()  
+            
             if ('G1' in lastLine or 'G0'in lastLine) and 'E' in lastLine:
                 command = lastLine.split(' ')
                 for part in command:
@@ -122,40 +127,44 @@ with open(args.filename, mode='r+t') as f:
                     if 'Y' in part:
                         yCoord = float(part[1:])
             
-        endOfLastLine = fplus.tell()
-
-#search for layer or height
+#search for layer or height 
         #if given height is reached or only slightly missed or if layer is reached
-        if (args.zheight and \
-           ( args.zheight >= height or (args.zheight - height )<= args.epsilon)) or \
-           ( args.layer and args.layer == layer):
-            break
+        if stop and \
+           ((args.zheight and \
+             ( args.zheight <= height or \
+              abs(args.zheight - height ) <= args.epsilon\
+             )\
+            ) or \
+            ( args.layer and args.layer == layer) \
+           ) :
+        
+            #insert extrusion set to last layers value
+            if extrude:
+                resetExtrusionCommand = "G92 E{0:.5f}\n".format(extrude)
+                w.write(resetExtrusionCommand)
+            else:
+                raise RuntimeWarning("No Extrusion value found.")
+            #insert post-init move to height + safety
+            gotoHeightCommand = "G0 X0 Y0 Z{0:.2f}\n".format(height+10)
+            w.write(gotoHeightCommand)                
+            #gotoStartCommand = "G0 X{0:.2f} Y{0:.2f} Z{0:.2f}".format(xCoord, yCoord, height+1)
+            #continue copying lines to new file
+            stop = False
 
-#insert extrusion set to last layers value
-resetExtrusionCommand = "G92 E{0:.5f}".format(extrude)
+    #copy each line to temp file
+        if not stop:
+            w.write(line)
 
-#insert post-init move to height + safety
-gotoHeightCommand = "G0 X0 Y0 Z{0:.2f}".format(height+10)
-
-gotoStartCommand = "G1 X{0:.2f} Y{0:.2f} Z{0:.2f}".format(xCoord, yCoord, height+1)
-
-
-print resetExtrusionCommand
-print gotoHeightCommand
-
-if startLayer0:
-    print startLayer0
-if cutPosition:
-    print cutPosition
-if extrude:
-    print xCoord 
-    print yCoord
-    print extrude
-if height:
-    print height
-    
-
-
-#delete non-wanted code
+if not layer: #LAYER:0 was not found 
+    w.close()
+    remove(temp_name)
+    exit("Nothing removed from file. Invalid content or parameters.")  
+      
 #save file
+# close temp file, copy over original, destroy temp    
+w.close()
+copy(temp_name, args.filename)
+remove(temp_name)
+
 #return x,y of first point to calling program
+#print xCoord, yCoord
