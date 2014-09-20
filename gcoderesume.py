6e3c19974e3a5e5643e0a527bf661a08bb2e685c
Chaos <stefan@binaervarianz.de>
2014-09-20 17:45:53 +0200

    project files and start of arg in

---
+++ gcoderesume.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Read and modify Gcode')
+
+parser.add_argument('filename', action='store', type=argparse.FileType('a'), required=True, help='Gcode-File to be altered')
+
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-l', '--layer',  action='store', type=int, metavar='layer', help='The layer at which to continue printing')
+group.add_argument('-h', '--height', action='store', type=int, metavar='height', help='The height at which to continue printing')
+
+args = parser.parse_args()
+
+print args
