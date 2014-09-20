860d5aa0fd66d54b9293a14cf5f31df2fd79502f
Chaos <stefan@binaervarianz.de>
2014-09-20 17:49:32 +0200

    fixed parsing

--- gcoderesume.py
+++ gcoderesume.py
@@ -4,11 +4,11 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Read and modify Gcode')
 
-parser.add_argument('filename', action='store', type=argparse.FileType('a'), required=True, help='Gcode-File to be altered')
+parser.add_argument('filename', action='store', type=argparse.FileType('a'), help='Gcode-File to be altered')
 
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('-l', '--layer',  action='store', type=int, metavar='layer', help='The layer at which to continue printing')
-group.add_argument('-h', '--height', action='store', type=int, metavar='height', help='The height at which to continue printing')
+group.add_argument('-z', '--zheight', action='store', type=int, metavar='height', help='The height at which to continue printing')
 
 args = parser.parse_args()
 
